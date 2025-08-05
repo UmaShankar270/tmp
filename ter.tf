088fd0e9f8d0c6c1e8b4696b3d7e6d49dd5e17bf
UmaShankar270 <umashankarayitipamula27@gmail.com>
2025-08-06 01:26:32 +0530

    Update ter.tf

--- ter.tf
+++ ter.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "example" {
  
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/24"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
@@ -65,3 +65,4 @@ resource "azurerm_virtual_machine" "example" {
     disable_password_authentication = false
   }
 }
+
